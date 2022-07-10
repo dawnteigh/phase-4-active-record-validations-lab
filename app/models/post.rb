01faa063085d4d311c061649e27c32d0135ab903
Donte Ladatto <donte279@gmail.com>
2022-07-09 22:58:28 -0500

    commit tree 29dafc7bf98253d2d4ce67b0022daec443923c24

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+
+
+    private
+
+    def title_is_clickbait
+        if title == "True Facts"
+            errors.add(:title, "NO CLICKBAIT!")
+        end
+    end
 end
